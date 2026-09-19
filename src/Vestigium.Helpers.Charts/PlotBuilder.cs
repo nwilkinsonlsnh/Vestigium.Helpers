caf1c9154028a5ebe9f63d5dcaa06a8720c3876b
Zazoo <155839158+nwilkinsonlsnh@users.noreply.github.com>
2026-09-19 14:48:19 -0400

    fix(charts): PlotBuilder is partial; FillControl lives in PlotBuilder.Control.cs

--- src/Vestigium.Helpers.Charts/PlotBuilder.cs
+++ src/Vestigium.Helpers.Charts/PlotBuilder.cs
@@ -3,7 +3,7 @@ using Vestigium.Helpers.Analytics;
 
 namespace Vestigium.Helpers.Charts;
 
-internal static class PlotBuilder
+internal static partial class PlotBuilder
 {
     public static Plot Create(ChartSpec spec)
     {
@@ -351,51 +351,6 @@ internal static class PlotBuilder
         err.Color = Color.FromHex(Palette.Trend);
         err.LegendText = $"{gamma:P0} CI";
         plot.Axes.SetLimitsX(0, 2);
-    }
-
-    private static void FillControl(Plot plot, ChartSpec spec, ChartOptions options)
-    {
-        var limits = spec.Limits ?? options.Limits
-            ?? throw new ArgumentNullException(nameof(spec), "Control chart requires ControlLimits from Analytics.");
-        var (xs, ys) = Xy(spec);
-        var inX = new List<double>();
-        var inY = new List<double>();
-        var outX = new List<double>();
-        var outY = new List<double>();
-        for (var i = 0; i < ys.Length; i++)
-        {
-            if (limits.IsOutOfControl(ys[i]))
-            {
-                outX.Add(xs[i]);
-                outY.Add(ys[i]);
-            }
-            else
-            {
-                inX.Add(xs[i]);
-                inY.Add(ys[i]);
-            }
-        }
-
-        var line = plot.Add.ScatterLine(xs, ys);
-        line.Color = Primary(options);
-        line.LegendText = spec.Source?.Name ?? "sample";
-        if (inX.Count > 0)
-        {
-            var ok = plot.Add.Scatter(inX, inY);
-            ok.Color = Primary(options);
-            ok.LegendText = "in control";
-        }
-
-        if (outX.Count > 0)
-        {
-            var bad = plot.Add.Scatter(outX, outY);
-            bad.Color = Color.FromHex(Palette.Outlier);
-            bad.LegendText = "outside";
-        }
-
-        AddHLine(plot, limits.Center, Palette.Cl, "CL");
-        AddHLine(plot, limits.Upper, Palette.Ucl, "UCL");
-        AddHLine(plot, limits.Lower, Palette.Lcl, "LCL");
     }
 
     private static void ApplyLimits(Plot plot, ControlLimits? limits, double[] xs, double[] ys)
